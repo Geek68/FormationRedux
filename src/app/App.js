b1a0d2d73f4143b7de5b9bd6c9c82b59f026bd01
Geek68 <njatovohyacinthe@gmail.com>
2024-08-11 18:44:11 +0300

    partie 2 chapitre 2

--- src/app/App.js
+++ src/app/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import { store } from './store';
 import { Cart } from '../features/Cart/Cart';
 import {TotalOrders} from '../features/Total/Total'
+import { Voucher } from '../features/Voucher/voucher';
+import { Owner } from '../features/Owner/Owner';
 function App() {
   return (
     <Provider store={store}>
         <div>
          <Cart/>
          <TotalOrders/>
+         <Voucher/>
+         <Owner/>
         </div>
     </Provider>
   );
